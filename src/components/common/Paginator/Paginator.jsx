13d791d0643fe9e5c70cfff24b82730bfc895982
Ilya Taldykin <rock_cell@mail.ru>
2021-03-07 13:10:18 +0300

    feat: add Paginator.jsx fixes

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -2,14 +2,28 @@ import React from 'react'
 import s from '../../Friends/FriendsPresentation/FriendsPresentation.module.css'
 import {NavLink} from "react-router-dom";
 
-function Paginator(props) {
-    let pagesCount = Math.ceil(props.totalCount / props.pageSize);
+const Paginator = React.memo((props) => {
+    //pagesCount should be %2=1
+    const middlePage = Math.ceil(props.pagesCount / 2);
+    const pagesCount = Math.ceil(props.totalCount / props.pageSize);
     let currentPage = props.currentPage ? props.currentPage : 1;
     if (currentPage > pagesCount) {
         currentPage = pagesCount;
     }
-    let pagesView;
-
+    let pagesView = [];
+    if (currentPage >= 1 && currentPage < middlePage) {
+        for (let i = 1; i <= props.pagesCount; i++) {
+            pagesView.push(i);
+        }
+    } else if (currentPage >= middlePage && currentPage <= (pagesCount - middlePage + 1)) {
+        for (let i = (currentPage - middlePage + 1); i <= (currentPage + middlePage - 1); i++) {
+            pagesView.push(i);
+        }
+    } else if (currentPage > (pagesCount - middlePage + 1) && currentPage <= pagesCount) {
+        for (let i = pagesCount - props.pagesCount; i <= pagesCount; i++) {
+            pagesView.push(i);
+        }
+    }
 
     return (
         <div className={s.pages}>
@@ -25,6 +39,7 @@ function Paginator(props) {
             })}
         </div>
     );
-}
+});
+
 
 export default Paginator;
